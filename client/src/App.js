cf27f369ad1ccb0a67f47013ca2cf2ae35c4fc83
Dariusan3 <dariusosadici@gmail.com>
2023-04-01 20:26:20 +0300

    Merge remote-tracking branch 'origin/frontend' into frontend

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import "./App.css";
 import Login from "./pages/Login/Login.js";
 import Signup from "./pages/Signup/Signup.js";
 import MyForm from "./pages/Signup/test";
+import ProfilePage from "./pages/ProfilePage/ProfilePage";
 function App() {
   return (
     <>
