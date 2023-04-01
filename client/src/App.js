5721f95b0730cdf5376e3fcf959d500a3da8f743
Dariusan3 <dariusosadici@gmail.com>
2023-04-01 19:22:35 +0300

    ProfilePage designed more for NewEmployee

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,8 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
-// import Login from "./pages/Login/Login.js";
+import Login from "./pages/Login/Login.js";
 import Signup from "./pages/Signup/Signup.js";
-<<<<<<< HEAD
 import PresPage from "./pages/PresentationPage/PresPage.js"
 import FeedPage from "./pages/FeedPage/FeedPage.js";
 import ProfilePage from "./pages/ProfilePage/ProfilePage.js";
@@ -11,20 +10,12 @@ import ProfilePage from "./pages/ProfilePage/ProfilePage.js";
 function App() {
   return (
     <>
-    {/* <Login/>
-    <Signup/> */}
+    {/* <Login/> */}
+    {/* <Signup/> */}
     {/* <FeedPage /> */}
-    <ProfilePage />
-=======
-import MyForm from "./pages/Signup/test";
-function App() {
-  return (
-    <>
-      {/* <Login /> */}
-      <Signup />
-      {/* <MyForm /> */}
->>>>>>> frontend
+     <ProfilePage /> 
     </>
   );
+
 }
 export default App;
--- client/src/pages/ProfilePage/ProfilePage.js
+++ client/src/pages/ProfilePage/ProfilePage.js
@@ -1,12 +1,101 @@
 import React from "react";
+import { useState, useEffect } from "react";
 import { Container, Row, Col, Image, Button } from "react-bootstrap";
 import AboutMe from "../../components/AboutMe.js";
 
 const ProfilePage = () => {
+
+  const [applicationStatus, setApplicationStatus] = useState({
+    status: "Matched",
+    matchedEmployee: null
+  });
+
+
+   // Helper functions for calculating match scores
+   const calculateWorkExperienceScore = (existingExperience, newExperience) => {
+    const experienceDifference = Math.abs(existingExperience - newExperience);
+    return 1 - (experienceDifference / 10); // normalize to a score between 0 and 1
+  };
+
+  const calculateTechnicalSkillsScore = (existingSkills, newSkills) => {
+    const matchedSkills = existingSkills.filter(skill => newSkills.includes(skill));
+    return matchedSkills.length / newSkills.length; // normalize to a score between 0 and 1
+  };
+
+  const calculateSoftSkillsScore = (existingSkills, newSkills) => {
+    const matchedSkills = existingSkills.filter(skill => newSkills.includes(skill));
+    return matchedSkills.length / newSkills.length; // normalize to a score between 0 and 1
+  };
+
+        const matchEmployee = () => {
+          const existingEmployees = [
+            {
+              id: 1,
+              name: "John",
+              workExperience: 5,
+              techStack: ["React", "JavaScript"],
+              softSkills: ["communication", "teamwork"]
+            },
+            {
+              id: 2,
+              name: "Jane",
+              workExperience: 3,
+              techStack: ["Java", "Python"],
+              softSkills: ["adaptability", "problem-solving"]
+            },
+            // add more employees as needed
+          ];
+        // Collect the new employee's skills and attributes
+        const newEmployeeSkills = {
+          workExperience: 2, // years of work experience
+          technicalSkills: ['JavaScript', 'React', 'Node.js'], // array of technical skills
+          softSkills: ['communication', 'teamwork', 'problem-solving'] // array of soft skills
+        };
+
+
+        // Find the existing employee with the closest match to the new employee
+        let matchedEmployee = null;
+        let highestMatchScore = 0;
+        for (let i = 0; i < existingEmployees.length; i++) {
+          const existingEmployee = existingEmployees[i];
+
+          // Calculate the match score based on work experience, technical skills, and soft skills
+          const matchScore = (
+            calculateWorkExperienceScore(existingEmployee.workExperience, newEmployeeSkills.workExperience) +
+            calculateTechnicalSkillsScore(existingEmployee.technicalSkills, newEmployeeSkills.technicalSkills) +
+            calculateSoftSkillsScore(existingEmployee.softSkills, newEmployeeSkills.softSkills)
+          );
+
+          // Update the matched employee if the match score is higher than the current highest score
+          if (matchScore > highestMatchScore) {
+            matchedEmployee = existingEmployee;
+            highestMatchScore = matchScore;
+          }
+        }
+
+        // Update the new employee's application status with the matched employee
+        const newApplicationStatus = {
+          status: "Matched",
+          matchedEmployee: matchedEmployee
+        };
+        setApplicationStatus(newApplicationStatus);
+      };
+
+      if(applicationStatus === "Pending"){
+        console.log("am intrat");
+        matchEmployee();
+      }
+
+     
+
+      /*useEffect(() =>{
+        console.log(applicationStatus);
+      })*/
+
   return (
     <Container>
+
       <Row>
-        
         <Col md={{ span: 8, offset: 2 }}>
           <AboutMe />
           <div className="text-center">
@@ -44,6 +133,25 @@ const ProfilePage = () => {
             <li>Node.js</li>
             <li>HTML/CSS</li>
           </ul>
+        
+             <div className="mt-4">
+             <h4>Application Status</h4>
+             {applicationStatus.status === "Pending" ? (
+               <p>Your application is currently being processed. Please wait for a response.</p>
+             ) : applicationStatus.status === "Matched" ? (
+               <div>
+                 <p>You have been matched with:</p>
+                 <ul>
+                   <li>Name: Jane Doe</li>
+                   <li>Occupation: Senior Software Engineer</li>
+                   <li>Skills: React, Node.js, Leadership</li>
+                 </ul>
+               </div>
+             ) : (
+               <p>Unknown application status.</p>
+             )}
+           </div>
+      
         </Col>
       </Row>
     </Container>
