818a81508aef924dc01b8493985cf41a63da201f
Miro <miroslav.maletici@student.upt.ro>
2023-04-01 20:25:46 +0300

    semifinishsignup

--- client/src/pages/Signup/Signup.js
+++ client/src/pages/Signup/Signup.js
@@ -6,6 +6,7 @@ import Dropdown from "react-bootstrap/Dropdown";
 import Persdate from "./personaldate";
 import Signups from "./test";
 import React, { useState } from "react";
+import { InputGroup } from "react-bootstrap";
 
 const Signup = () => {
   const [selectedOption, setSelectedOption] = useState("Select an option");
@@ -13,10 +14,20 @@ const Signup = () => {
   function handleOptionSelect(eventKey, event) {
     setSelectedOption(event.target.innerText);
   }
-  const [selectedOptions, setSelectedOptions] = useState("Choose one");
+  const [selectedOptions, setSelectedOptions] = useState("Select an option");
 
   const handleDropdownSelect = (eventKey, event) => {
     setSelectedOptions(event.target.innerHTML);
+  };
+  const [selectedOption1, setSelectedOption1] = useState("");
+
+  const handleradioSelect = (event) => {
+    setSelectedOption1(event.target.value);
+  };
+
+  const [selectedOptions2, setSelectedOptions2] = useState("Select an option");
+  const handleDropdown2Select = (eventKey, event) => {
+    setSelectedOptions2(event.target.innerHTML);
   };
 
   return (
@@ -75,34 +86,209 @@ const Signup = () => {
                 </Dropdown>
               </Form.Group>
 
-              <Form.Group controlId="formBasicPassword">
-                <Form.Label>Password</Form.Label>
-                <Form.Control type="password" placeholder="Password" />
-              </Form.Group>
-              <Form.Group controlId="formBasicPassword">
-                <Form.Label>Confirm Password</Form.Label>
-                <Form.Control type="password" placeholder="Confirm Password" />
-              </Form.Group>
               <Form>
-                <Form.Label>
-                  Are you more confortable with Frontend or backend?
-                </Form.Label>
-                {["radio"].map((type) => (
-                  <div key={`${type}`} className="mb-3">
-                    <Form.Check
-                      label="Frontend"
-                      name="group1"
-                      type={type}
-                      id={`${type}-1`}
-                    />
-                    <Form.Check
-                      label="Backend"
-                      name="group1"
-                      type={type}
-                      id={`${type}-2`}
-                    />
-                  </div>
-                ))}
+                <Form.Group controlId="formBasicRadio">
+                  <Form.Check
+                    type="radio"
+                    label="Frontend"
+                    name="radio"
+                    value="Frontend"
+                    onChange={handleradioSelect}
+                  />
+                  <Form.Check
+                    type="radio"
+                    label="Backend"
+                    name="radio"
+                    value="Backend"
+                    onChange={handleradioSelect}
+                  />
+                </Form.Group>
+
+                {selectedOption1 === "Frontend" && (
+                  <>
+                    <Form.Group controlId="formBasicFrontend">
+                      <Form.Label>
+                        Whitch Frontend framework are you comfortable with?
+                      </Form.Label>
+                      <Form.Check label="React" value="Frontend" />
+                      <Form.Check label="Angular" value="Frontend" />
+                      <Form.Check label="Vue" value="Frontend" />
+                    </Form.Group>
+                    <Form.Label>
+                      On a scale from 1 to 5,how familiar are you in the
+                      following programing language?
+                    </Form.Label>
+                    <Form.Group
+                      as={Col}
+                      md="4"
+                      controlId="validationCustomUsername"
+                    >
+                      <InputGroup hasValidation>
+                        <InputGroup.Text id="inputGroupPrepend">
+                          Javascript
+                        </InputGroup.Text>
+                        <Form.Control
+                          type="text"
+                          placeholder="Value"
+                          aria-describedby="inputGroupPrepend"
+                        />
+                      </InputGroup>
+                      <InputGroup hasValidation>
+                        <InputGroup.Text id="inputGroupPrepend">
+                          TypeScript
+                        </InputGroup.Text>
+                        <Form.Control
+                          type="text"
+                          placeholder="Value"
+                          aria-describedby="inputGroupPrepend"
+                        />
+                      </InputGroup>
+                      <InputGroup hasValidation>
+                        <InputGroup.Text id="inputGroupPrepend">
+                          HTML
+                        </InputGroup.Text>
+                        <Form.Control
+                          type="text"
+                          placeholder="Value"
+                          aria-describedby="inputGroupPrepend"
+                        />
+                      </InputGroup>
+                      <InputGroup hasValidation>
+                        <InputGroup.Text id="inputGroupPrepend"></InputGroup.Text>
+                        <Form.Control
+                          type="text"
+                          placeholder="Value"
+                          aria-describedby="inputGroupPrepend"
+                        />
+                      </InputGroup>
+                    </Form.Group>
+                    <Form.Group controlId="formBasicFrontend">
+                      <Form.Label>
+                        Whitch Frontend tools are you comfortable with?
+                      </Form.Label>
+                      <Form.Check label="Webpack" value="Frontend" />
+                      <Form.Check label="Babel" value="Frontend" />
+                      <Form.Check label="Grunt/Gulp" value="Frontend" />
+                      <Form.Check label="NPM/Yarn" value="Frontend" />
+                    </Form.Group>
+                  </>
+                )}
+
+                {selectedOption1 === "Backend" && (
+                  <>
+                    <Form.Group controlId="formBasicFrontend">
+                      <Form.Label>
+                        Whitch Backend framework are you comfortable with?
+                      </Form.Label>
+                      <Form.Check label="Node.js" value="Frontend" />
+                      <Form.Check label="Django" value="Frontend" />
+                      <Form.Check label="Ruby on rails" value="Frontend" />
+                      <Form.Check label="Spring Boot" value="Frontend" />
+                      <Form.Check label="ASP.NET" value="Frontend" />
+                    </Form.Group>
+                    <Form.Group controlId="formBasicFrontend">
+                      <Form.Label>
+                        Whitch programing language are you familiar with?
+                      </Form.Label>
+                      <Form.Check label="Java" value="Frontend" />
+                      <Form.Check label="Python" value="Frontend" />
+                      <Form.Check label="Ruby" value="Frontend" />
+                      <Form.Check label="PHP" value="Frontend" />
+                      <Form.Check label="C#" value="Frontend" />
+                    </Form.Group>
+                    <Form.Group controlId="formBasicFrontend">
+                      <Form.Label>
+                        On a scale from 1 to 5,how familiar are you in the
+                        following Backend Tools?
+                      </Form.Label>
+                      <InputGroup hasValidation>
+                        <InputGroup.Text id="inputGroupPrepend">
+                          Docker
+                        </InputGroup.Text>
+                        <Form.Control
+                          type="text"
+                          placeholder="Value"
+                          aria-describedby="inputGroupPrepend"
+                        />
+                      </InputGroup>
+                      <InputGroup hasValidation>
+                        <InputGroup.Text id="inputGroupPrepend">
+                          Kubernetes
+                        </InputGroup.Text>
+                        <Form.Control
+                          type="text"
+                          placeholder="Value"
+                          aria-describedby="inputGroupPrepend"
+                        />
+                      </InputGroup>
+                      <InputGroup hasValidation>
+                        <InputGroup.Text id="inputGroupPrepend">
+                          MySQL
+                        </InputGroup.Text>
+                        <Form.Control
+                          type="text"
+                          placeholder="Value"
+                          aria-describedby="inputGroupPrepend"
+                        />
+                      </InputGroup>
+                      <InputGroup hasValidation>
+                        <InputGroup.Text id="inputGroupPrepend">
+                          AWS/AZURE/GCP
+                        </InputGroup.Text>
+                        <Form.Control
+                          type="text"
+                          placeholder="Value"
+                          aria-describedby="inputGroupPrepend"
+                        />
+                      </InputGroup>
+                    </Form.Group>
+                  </>
+                )}
+                <Form.Group controlId="formBasicPassword">
+                  <Form.Label>What is your comunication style? </Form.Label>
+                  <Dropdown onSelect={handleDropdown2Select}>
+                    <Dropdown.Toggle variant="primary" id="dropdown-basic">
+                      {selectedOptions2}
+                    </Dropdown.Toggle>
+
+                    <Dropdown.Menu>
+                      <Dropdown.Item eventKey="Assertive">
+                        Assertive
+                      </Dropdown.Item>
+                      <Dropdown.Item eventKey="Passive">Passive</Dropdown.Item>
+                      <Dropdown.Item eventKey="Aggressive">
+                        Aggressive
+                      </Dropdown.Item>
+                      <Dropdown.Item eventKey="Collaborative">
+                        Collaborative
+                      </Dropdown.Item>
+                    </Dropdown.Menu>
+                  </Dropdown>
+                </Form.Group>
+                <Form.Group controlId="formBasicPassword">
+                  <Form.Label>How do you handle in with conflicts? </Form.Label>
+                  <Dropdown onSelect={handleDropdown2Select}>
+                    <Dropdown.Toggle variant="primary" id="dropdown-basic">
+                      {selectedOptions2}
+                    </Dropdown.Toggle>
+
+                    <Dropdown.Menu>
+                      <Dropdown.Item eventKey="Assertive">
+                        Avoidance
+                      </Dropdown.Item>
+                      <Dropdown.Item eventKey="Passive">Passive</Dropdown.Item>
+                      <Dropdown.Item eventKey="Aggressive">
+                        Confrontation
+                      </Dropdown.Item>
+                      <Dropdown.Item eventKey="Collaborative">
+                        Compromise
+                      </Dropdown.Item>
+                      <Dropdown.Item eventKey="Collaborative">
+                        Collaboration
+                      </Dropdown.Item>
+                    </Dropdown.Menu>
+                  </Dropdown>
+                </Form.Group>
               </Form>
             </Form>
           </Col>
