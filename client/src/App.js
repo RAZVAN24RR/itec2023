f0d05f152a611d3280e3c4c9b4c41a6ed9348272
Miro <miroslav.maletici@student.upt.ro>
2023-04-01 18:35:36 +0300

    funcional dropbox

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 // import Login from "./pages/Login/Login.js";
 import Signup from "./pages/Signup/Signup.js";
-import MyForm from "./pages/Signup/test";
+
 function App() {
   return (
     <>
--- client/src/pages/Signup/Signup.js
+++ client/src/pages/Signup/Signup.js
@@ -1,52 +1,27 @@
-import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Dropdown from "react-bootstrap/Dropdown";
+import Persdate from "./personaldate";
+import Signups from "./test";
+import React, { useState } from "react";
 
 const Signup = () => {
+  const [selectedOption, setSelectedOption] = useState("Select an option");
+
+  function handleOptionSelect(eventKey, event) {
+    setSelectedOption(event.target.innerText);
+  }
+  const [selectedOptions, setSelectedOptions] = useState("Choose one");
+
+  const handleDropdownSelect = (eventKey, event) => {
+    setSelectedOptions(event.target.innerHTML);
+  };
+
   return (
     <>
-      <Container className="d-flex justify-content-center align-items-center vh-100">
-        <Row className="justify-content-center align-items-center h-100 container-fluid">
-          <Col md={6} className="bg-light p-5 rounded">
-            <h1 className="text-center mb-4">Personal Dates</h1>
-
-            <Form className="text-center">
-              <Form.Group controlId="formBasicPassword">
-                <Form.Label>Name </Form.Label>
-                <Form.Control type="password" placeholder="Name" />
-              </Form.Group>
-              <Form.Group controlId="formBasicEmail">
-                <Form.Label>Email address</Form.Label>
-                <Form.Control type="email" placeholder="Enter email" />
-                <Form.Text className="text-muted">
-                  We'll never share your email with anyone else.
-                </Form.Text>
-              </Form.Group>
-
-              <Form.Group controlId="formBasicPassword">
-                <Form.Label>Password</Form.Label>
-                <Form.Control type="password" placeholder="Password" />
-              </Form.Group>
-              <Form.Group controlId="formBasicPassword">
-                <Form.Label>Confirm Password</Form.Label>
-                <Form.Control type="password" placeholder="Confirm Password" />
-              </Form.Group>
-
-              <Form.Group controlId="formBasicCheckbox">
-                <Form.Check type="checkbox" label="Check me out" />
-              </Form.Group>
-
-              <Button variant="primary" size="lg" type="submit" block>
-                Sign up{" "}
-              </Button>
-            </Form>
-          </Col>
-        </Row>
-      </Container>
-
+      <Persdate />
       <Container className="d-flex justify-content-center align-items-center vh-100">
         <Row className="justify-content-center align-items-center h-100 container-fluid">
           <Col md={6} className="bg-light p-5 rounded">
@@ -57,17 +32,25 @@ const Signup = () => {
                 <Form.Label>
                   How many years of work Experience do you have?{" "}
                 </Form.Label>
-                <Dropdown>
+                <Dropdown onSelect={handleOptionSelect}>
                   <Dropdown.Toggle variant="primary" id="dropdown-basic">
-                    Years
+                    {selectedOption}
                   </Dropdown.Toggle>
 
                   <Dropdown.Menu>
-                    <Dropdown.Item href="#/action-1">0-1 years</Dropdown.Item>
-                    <Dropdown.Item href="#/action-2">1-3 years</Dropdown.Item>
-                    <Dropdown.Item href="#/action-3">3-5 years</Dropdown.Item>
-                    <Dropdown.Item href="#/action-4">5-10 years</Dropdown.Item>
-                    <Dropdown.Item href="#/action-5">
+                    <Dropdown.Item eventKey="0-1 years">
+                      0-1 years
+                    </Dropdown.Item>
+                    <Dropdown.Item eventKey="1-3 years">
+                      1-3 years
+                    </Dropdown.Item>
+                    <Dropdown.Item eventKey="3-5 years">
+                      3-5 years
+                    </Dropdown.Item>
+                    <Dropdown.Item eventKey="5-10 years">
+                      5-10 years
+                    </Dropdown.Item>
+                    <Dropdown.Item eventKey="more than 10 years">
                       more than 10 years
                     </Dropdown.Item>
                   </Dropdown.Menu>
@@ -75,17 +58,19 @@ const Signup = () => {
               </Form.Group>
               <Form.Group controlId="formBasicEmail">
                 <Form.Label>In what industry have you work?</Form.Label>
-                <Dropdown>
+                <Dropdown onSelect={handleDropdownSelect}>
                   <Dropdown.Toggle variant="primary" id="dropdown-basic">
-                    Choose one
+                    {selectedOptions}
                   </Dropdown.Toggle>
 
                   <Dropdown.Menu>
-                    <Dropdown.Item href="#/action-1">Tehnology</Dropdown.Item>
-                    <Dropdown.Item href="#/action-2">Healthcare</Dropdown.Item>
-                    <Dropdown.Item href="#/action-3">Finance</Dropdown.Item>
-                    <Dropdown.Item href="#/action-4">Education</Dropdown.Item>
-                    <Dropdown.Item href="#/action-5">Retail</Dropdown.Item>
+                    <Dropdown.Item eventKey="Tehnology">
+                      Tehnology
+                    </Dropdown.Item>
+                    <Dropdown.Item eventKey="2">Healthcare</Dropdown.Item>
+                    <Dropdown.Item eventKey="3">Finance</Dropdown.Item>
+                    <Dropdown.Item eventKey="4">Education</Dropdown.Item>
+                    <Dropdown.Item eventKey="5">Retail</Dropdown.Item>
                   </Dropdown.Menu>
                 </Dropdown>
               </Form.Group>
@@ -123,6 +108,7 @@ const Signup = () => {
           </Col>
         </Row>
       </Container>
+      <Signups />
     </>
   );
 };
