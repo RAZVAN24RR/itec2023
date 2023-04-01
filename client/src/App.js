cb6c455c465e8124000d15474e873d0bdacfd7d7
Dariusan3 <dariusosadici@gmail.com>
2023-04-01 16:29:43 +0300

    Feed page + Questions component

--- client/src/App.js
+++ client/src/App.js
@@ -4,13 +4,14 @@ import "./App.css";
 import Login from "./pages/Login/Login.js";
 import Signup from "./pages/Signup/Signup.js";
 import PresPage from "./pages/PresentationPage/PresPage.js"
+import FeedPage from "./pages/FeedPage/FeedPage.js";
 
 function App() {
   return (
     <>
     {/* <Login/>
     <Signup/> */}
-    <PresPage />
+    <FeedPage />
     </>
   );
 
