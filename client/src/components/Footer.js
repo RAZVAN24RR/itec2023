4ad12725d6531f3d8949e0bcfbb14c3ae1da017b
Dariusan3 <dariusosadici@gmail.com>
2023-04-01 21:07:12 +0300

    Footer done i think

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -5,73 +5,31 @@ function Footer() {
   return (
     <footer className="bg-light fixed-bottom">
       <Container>
-        <Row>
-          <Col lg={3} md={6}>
-            <h5>About Us</h5>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ut
-              velit id tellus ullamcorper iaculis sit amet ac ligula.
-            </p>
-          </Col>
-          <Col lg={3} md={6}>
-            <h5>Links</h5>
-            <ul className='list-unstyled'>
-              <li>
-                <a href='/'>Home</a>
-              </li>
-              <li>
-                <a href='/about'>About Us</a>
-              </li>
-              <li>
-                <a href='/services'>Services</a>
-              </li>
-              <li>
-                <a href='/contact'>Contact Us</a>
-              </li>
-            </ul>
-          </Col>
-          <Col lg={3} md={6}>
-            <h5>Subscribe</h5>
-            <p>
-              Subscribe to our newsletter and stay up to date with our latest
-              news and updates.
-            </p>
-            <form>
-              <div className='form-floating mb-3'>
-                <input
-                  type='email'
-                  className='form-control'
-                  id='newsletterEmail'
-                  placeholder='Enter your email'
-                />
-                <label htmlFor='newsletterEmail'>Email address</label>
-              </div>
-              <button className='btn btn-primary' type='submit'>
-                Subscribe
-              </button>
-            </form>
-          </Col>
-          <Col lg={3} md={6}>
-            <h5>Contact Us</h5>
-            <p>
-              <i className='bi bi-geo-alt'></i> 123 Main St, New York, NY 10001
-            </p>
-            <p>
-              <i className='bi bi-telephone'></i> (555) 555-5555
-            </p>
-            <p>
-              <i className='bi bi-envelope'></i> info@example.com
-            </p>
-          </Col>
-        </Row>
-        <hr />
-        <div className='d-flex justify-content-between align-items-center'>
-          <div>
-            <p>
-              &copy; {new Date().getFullYear()} MyCompany. All rights reserved.
-            </p>
+        <Row  style={{paddingTop:'4rem'}}>
+          <div className="d-flex justify-content-between">
+              <Col md={6}>
+                  <h5>About Us</h5>
+                  <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ut
+                    velit id tellus ullamcorper iaculis sit amet ac ligula.
+                  </p>
+              </Col>
+              <Col md={{ span: 10, offset: 3 }}>
+                  <h5>Contact Us</h5>
+                  <p>
+                    <i className='bi bi-geo-alt'></i> 123 Main St, New York, NY 10001
+                  </p>
+                  <p>
+                    <i className='bi bi-telephone'></i> (555) 555-5555
+                  </p>
+                  <p>
+                    <i className='bi bi-envelope'></i> info@example.com
+                  </p>
+              </Col>
           </div>
-          <div>
+        </Row>
+        <Row>
+          <Col lg={12}>
             <ul className='list-unstyled list-inline'>
               <li className='list-inline-item'>
                 <a href='/'>Terms &amp; Conditions</a>
@@ -80,8 +38,15 @@ function Footer() {
                 <a href='/'>Privacy Policy</a>
               </li>
             </ul>
-          </div>
-        </div>
+          </Col>
+        </Row>
+        <Row>
+          <Col lg={12}>
+            <p>
+              &copy; {new Date().getFullYear()} MyCompany. All rights reserved.
+            </p>
+          </Col>
+        </Row>
       </Container>
     </footer>
   );
