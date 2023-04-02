7aacc74f03ca7e3b5e7924a1556158ff1aa3c83c
Miro <miroslav.maletici@student.upt.ro>
2023-04-02 07:36:51 +0300

    last

--- client/src/components/Questions.js
+++ client/src/components/Questions.js
@@ -1,58 +1,118 @@
-import Accordion from 'react-bootstrap/Accordion';
-import Container from 'react-bootstrap/Container';
+import Accordion from "react-bootstrap/Accordion";
+import Container from "react-bootstrap/Container";
 
 function Questions() {
   return (
+<<<<<<< Updated upstream
     <Container fluid className="d-flex flex-column align-items-center justify-content-center vh-100" style={{maxHeight: '450px', minHeight: '300px', minWidth: '150px', maxWidth: '500px'}}>
       <Accordion className="w-100" style={{paddingTop:'200px'}}>
+=======
+    <Container
+      fluid
+      className="d-flex flex-column align-items-center justify-content-center margin"
+      style={{
+        maxHeight: "450px",
+        minHeight: "300px",
+        minWidth: "150px",
+        maxWidth: "1200px",
+        paddingTop: "150px",
+      }}
+    >
+      <Accordion className="w-100  justify-content-center align-items-center margin">
+>>>>>>> Stashed changes
         <Accordion.Item eventKey="0">
           <Accordion.Header>
-            <h5>1. Intrebare</h5>
+            <h5>1. Cum as putea sa devin Buddy?</h5>
           </Accordion.Header>
           <Accordion.Body>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum.
           </Accordion.Body>
         </Accordion.Item>
         <Accordion.Item eventKey="1">
           <Accordion.Header>
-            <h5>2. Intrebare</h5>
+            <h5>2. Ce atributi am ca New Employee?</h5>
           </Accordion.Header>
           <Accordion.Body>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum.
           </Accordion.Body>
         </Accordion.Item>
         <Accordion.Item eventKey="2">
           <Accordion.Header>
-          <h5>3. Intrebare</h5>
+            <h5>3. Cum pot discuta cu manager-ul meu?</h5>
           </Accordion.Header>
           <Accordion.Body>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do ei
           </Accordion.Body>
         </Accordion.Item>
         <Accordion.Item eventKey="3">
           <Accordion.Header>
+<<<<<<< Updated upstream
           <h5>3. Intrebare</h5>
           </Accordion.Header>
           <Accordion.Body>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+=======
+            <h5>4. Cum aflu care imi sunt colegii de echipa? </h5>
+          </Accordion.Header>
+          <Accordion.Body>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+>>>>>>> Stashed changes
           </Accordion.Body>
         </Accordion.Item>
         <Accordion.Item eventKey="4">
           <Accordion.Header>
+<<<<<<< Updated upstream
           <h5>3. Intrebare</h5>
           </Accordion.Header>
           <Accordion.Body>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+=======
+            <h5>5.Care imi sunt atributiile?</h5>
+          </Accordion.Header>
+          <Accordion.Body>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+>>>>>>> Stashed changes
           </Accordion.Body>
         </Accordion.Item>
         <Accordion.Item eventKey="5">
           <Accordion.Header>
+<<<<<<< Updated upstream
           <h5>3. Intrebare</h5>
           </Accordion.Header>
           <Accordion.Body>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
           </Accordion.Body>
         </Accordion.Item>
+=======
+            <h5>6.Daca am o intrebare despre job, cui o adresez? </h5>
+          </Accordion.Header>
+          <Accordion.Body>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+          </Accordion.Body>
+        </Accordion.Item>
+        <Accordion.Item eventKey="6">
+          <Accordion.Header>
+            <h5>
+              7. Care este numarul maxim de New Employee pe care pot sa ii iau?
+            </h5>
+          </Accordion.Header>
+          <Accordion.Body>Ana are mere</Accordion.Body>
+        </Accordion.Item>
+>>>>>>> Stashed changes
       </Accordion>
     </Container>
   );
--- client/src/pages/homepage/home.js
+++ client/src/pages/homepage/home.js
@@ -7,8 +7,13 @@ import Footer from "../../components/Footer/Footer";
 import "./home.css";
 import logo from "../../assets/logo.jpg";
 import { Nav, Navbar } from "react-bootstrap";
+<<<<<<< Updated upstream
 import Questions from "../../components/Questions.js";
 
+=======
+import MyFaq from "./myfaq";
+import Questions from "../../components/Questions";
+>>>>>>> Stashed changes
 const Home = () => {
   return (
     <>
@@ -29,8 +34,13 @@ const Home = () => {
         </Container>
       </Navbar>
       {/* Navbar End */}
+<<<<<<< Updated upstream
 
       <Questions />
+=======
+      <Questions />
+      <p className="padingg"></p>
+>>>>>>> Stashed changes
     </>
   );
 };
